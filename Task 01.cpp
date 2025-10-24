d2e9e87c300b0813ee3026d0b475608702884412
Dua-Tariq0104 <f2025-0104@bnu.edu.pk>
2025-10-24 14:41:29 +0500

    Create Task 01.cpp

---
+++ Task 01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Name:\t\tDua Tariq\n";
+    cout << "Department:\tSCIT\n";
+    cout << "Quote:\t\t\"Knowledge is power.\"\n";
+    return 0;
+}
